853c02a70d45bf8aba4339aae3437bdec46d679e
Kathryn Hough <kathrynmhough@gmail.com>
2014-06-02 22:22:49 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('./package.json'),
+
+    connect: {
+      dev: {
+        options: {
+          port: 8000,
+          base: './dist/',
+          keepalive: true
+        }
+      }
+    },
+
+    /* assemble templating */
+    assemble: {
+      options: {
+        collections: [{
+          name: 'post',
+          sortby: 'posted',
+          sortorder: 'descending'
+        }],
+        helpers: './src/bonnet/helpers/**/*.js',
+        layout: 'page.hbs',
+        layoutdir: './src/bonnet/layouts/',
+        partials: './src/bonnet/partials/**/*'
+      },
+      posts: {
+        files: [{
+          cwd: './src/content/',
+          dest: './dist/',
+          expand: true,
+          src: ['**/*.hbs', '**/*.md', '!_pages/**/*.hbs']
+        }, {
+          cwd: './src/content/_pages/',
+          dest: './dist/',
+          expand: true,
+          src: '**/*.hbs'
+        }]
+      }
+    }
+  });
+
+  /* load every plugin in package.json */
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('assemble');
+
+  /* grunt tasks */
+  grunt.registerTask('default', ['assemble', 'connect']);
+
+};
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,7 @@
+---
+title: "First!"
+posted: 2014-06-02
+post: true
+---
+
+This is my first post!
